90508cdfb3a2e3b014157ab37ff4a28beb0407f1
duyhvt <thanhduy@Thanhs-MacBook-Pro.local>
2025-06-29 15:23:52 +0700

    AuctionService: Implement CRUD for Auction

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,8 +1,16 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -13,5 +21,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
